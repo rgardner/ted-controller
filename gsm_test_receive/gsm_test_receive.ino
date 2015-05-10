479b971c6d58daf7a92aa77f63e953964e4fb3f5
Robert Gardner <rhg259@nyu.edu>
2015-05-09 23:59:19 -0400

    Remove trailing whitespace and add comments

--- gsm_test_receive/gsm_test_receive.ino
+++ gsm_test_receive/gsm_test_receive.ino
@@ -1,23 +1,25 @@
 #include <SoftwareSerial.h>
 #include <String.h>
- 
+
 SoftwareSerial gprsSerial(10, 11);
 
 unsigned char buffer[64];  // buffer array for data receive over serial port
 int count = 0;  // counter for buffer array
- 
+
 void setup()
 {
-  gprsSerial.begin(19200); // GPRS shield baud rate 
+  gprsSerial.begin(19200); // GPRS shield baud rate
   Serial.begin(19200);
   delay(100);
+  // Read an SMS in index #1
   gprsSerial.print("AT+CMFGF=1\r");
   delay(100);
+  // Echo the contents of a new SMS upon receipt to serial out on GSM shield.
   gprsSerial.print("AT+CNMI=2,2,0,0,0\r");
   delay(100);
   Serial.println("Ready...");
 }
- 
+
 void loop() {
   //If a character comes in from the cellular module...
   if (gprsSerial.available() >0) {
